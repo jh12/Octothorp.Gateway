42ffe46f4b7c76eebddbb356d35f2c5235cc3c64
jh12 <jabaha35@hotmail.com>
2020-11-02 14:03:48 +0100

    fix: allow different path for configuration

--- Octothorp.Gateway/Program.cs
+++ Octothorp.Gateway/Program.cs
@@ -1,5 +1,7 @@
+using System;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
@@ -16,6 +18,22 @@ namespace Octothorp.Gateway
             Host.CreateDefaultBuilder(args)
                 .UseSerilog()
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureAppConfiguration(((context, builder) =>
+                {
+                    builder.Sources.Clear();
+
+                    IHostEnvironment env = context.HostingEnvironment;
+
+                    string? contentRootPath = Environment.GetEnvironmentVariable("contentroot");
+
+                    if (!string.IsNullOrEmpty(contentRootPath))
+                        builder.SetBasePath(contentRootPath);
+
+                    builder
+                        .AddEnvironmentVariables()
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}", optional: true, reloadOnChange: true);
+                }))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
